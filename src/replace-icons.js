9e627e0c0872ba422518e355c9b18a8210330d21
RafaelFantato <rafael.fantato@lcs.valantic.com>
2025-01-14 16:04:46 +0000

    config parser

--- src/replace-icons.js
+++ src/replace-icons.js
@@ -78,4 +78,16 @@ async function replaceIcons(context) {
     }
 }
 
+
+function getConfigParser(context, config) {
+
+    if (semver.lt(context.opts.cordova.version, '5.4.0')) {
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+    } else {
+        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+    }
+
+    return new ConfigParser(config);
+}
+
 module.exports = replaceIcons;
